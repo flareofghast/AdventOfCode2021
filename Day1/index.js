f3943afac75093fbbe7f315881c827c6a80e81da
Peter Salzmann <petersalzmann@gmail.com>
2021-12-03 13:09:32 +1030

    completed part 2 and refactor.

--- Day1/index.js
+++ Day1/index.js
@@ -2,20 +2,55 @@
 /* eslint-disable no-plusplus */
 const load = require('../common/loadfile');
 
-function ProcessInput(input) {
-  const data = input.split('\n');
+const filePath = '../Day1/input';
 
-  let NumIncreased = 0;
-
-  for (let i = 0, j = 1; j <= data.length; i++, j++) {
-    const num1 = parseInt(data[i]);
-    const num2 = parseInt(data[j]);
-    if (num1 < num2) {
-      NumIncreased += 1;
-    }
+function compareVals(val1, val2) {
+  let toReturn = 0;
+  if (val1 < val2) {
+    toReturn = 1;
   }
-
-  console.log(NumIncreased);
+  return toReturn;
 }
 
-load('../Day1/input', ProcessInput);
+function splitData(input) {
+  return input.split('\n');
+}
+
+function tallyIncreases(input) {
+  let NumIncreased = 0;
+
+  for (let i = 0, j = 1; j <= input.length; i++, j++) {
+    const num1 = parseInt(input[i]);
+    const num2 = parseInt(input[j]);
+
+    NumIncreased += compareVals(num1, num2);
+  }
+
+  return NumIncreased;
+}
+
+function ProcessInputPart1(input) {
+  const data = splitData(input);
+
+  console.log(`Part 1: ${tallyIncreases(data)}`);
+}
+
+function ProcessInputPart2(input) {
+  const data = splitData(input);
+  const slidingData = [];
+
+  for (let i = 0, j = 1, k = 2; k <= data.length; i++, j++, k++) {
+    const num1 = parseInt(data[i]);
+    const num2 = parseInt(data[j]);
+    const num3 = parseInt(data[k]);
+
+    const sum = num1 + num2 + num3;
+
+    slidingData.push(sum);
+  }
+
+  console.log(`Part 2: ${tallyIncreases(slidingData)}`);
+}
+
+load(filePath, ProcessInputPart1);
+load(filePath, ProcessInputPart2);
