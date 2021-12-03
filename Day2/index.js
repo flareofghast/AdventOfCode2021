d4279dc9c11414365bbfbb46a6b978c9f5711517
Peter Salzmann <petersalzmann@gmail.com>
2021-12-03 13:59:01 +1030

    Day 2 Part 2 completed

--- Day2/index.js
+++ Day2/index.js
@@ -26,7 +26,37 @@ function processInputPart1(input) {
     }
   });
 
-  console.log(horizontal * depth);
+  console.log(`Part 1: ${horizontal * depth}`);
+}
+
+function processInputPart2(input) {
+  const data = input.map((val) => val.split(' '));
+
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  data.forEach((val) => {
+    const parsed = parseInt(val[1]);
+
+    switch (val[0]) {
+      case 'forward':
+        horizontal += parsed;
+        depth += (parsed * aim);
+        break;
+      case 'down':
+        aim += parsed;
+        break;
+      case 'up':
+        aim -= parsed;
+        break;
+      default:
+        break;
+    }
+  });
+
+  console.log(`Part 2: ${horizontal * depth}`);
 }
 
 load(filePath, processInputPart1);
+load(filePath, processInputPart2);
