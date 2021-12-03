1650f1a3843258edda84f8ed06d78572257d377f
Peter Salzmann <petersalzmann@gmail.com>
2021-12-03 11:56:35 +1030

    adjusted to take in the input locally

--- common/loadfile.js
+++ common/loadfile.js
@@ -1,16 +1,17 @@
 // Load in a file from a URL
 
-const axios = require('axios');
-const { COOKIE } = require('./secrets');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = function (url, cb) {
-  let loaded;
-
-  axios(url, { headers: { Cookie: COOKIE } }, { withCredentials: true })
-    .then((res) => {
-      loaded = res.data;
-      cb(loaded);
-    }).catch((err) => {
-      console.log(err);
+  const file = path.resolve(__dirname, url);
+  try {
+    fs.readFile(file, 'utf8', (inErr, cont) => {
+      if (!inErr) {
+        cb(cont);
+      }
     });
+  } catch (fileErr) {
+    console.log(fileErr);
+  }
 };
