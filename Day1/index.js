e8a1bedbb6f54123a1d6f5d7acd39519e6915b0d
Peter Salzmann <petersalzmann@gmail.com>
2021-12-03 12:19:25 +1030

    completed first challenge

--- Day1/index.js
+++ Day1/index.js
@@ -1,8 +1,21 @@
+/* eslint-disable radix */
+/* eslint-disable no-plusplus */
 const load = require('../common/loadfile');
 
 function ProcessInput(input) {
   const data = input.split('\n');
-  console.log(data);
+
+  let NumIncreased = 0;
+
+  for (let i = 0, j = 1; j <= data.length; i++, j++) {
+    const num1 = parseInt(data[i]);
+    const num2 = parseInt(data[j]);
+    if (num1 < num2) {
+      NumIncreased += 1;
+    }
+  }
+
+  console.log(NumIncreased);
 }
 
-load('https://adventofcode.com/2021/day/1/input', ProcessInput);
+load('../Day1/input', ProcessInput);
