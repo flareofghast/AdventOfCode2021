86b5cc9423d1ac633328ed8f1e32ad62c445991a
Peter Salzmann <petersalzmann@gmail.com>
2021-12-09 07:33:47 +1030

    finally figured out day 5. Not the cleanest, but it does the job.

--- Day5/index.js
+++ Day5/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 const load = require('../common/loadfile');
 const CoordLine = require('./coordLine');
 
@@ -20,64 +21,162 @@ function findY(array, toFind) {
   return array.findIndex((a) => a.y === toFind);
 }
 
-function ProcessPart1(data) {
-  const input = splitCoods(data);
-
+function processHV(input) {
   const marked = [];
 
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < input.length; i++) {
-    const cur = input[i];
-    // check for vertical or horizontal only
-    if (cur.from.x === cur.to.x || cur.from.y === cur.to.y) {
-      const axis = cur.from.x === cur.to.x ? 'Y' : 'X';
-      const from = axis === 'Y' ? cur.from.y : cur.from.x;
-      const to = axis === 'Y' ? cur.to.y : cur.to.x;
-      // eslint-disable-next-line no-plusplus
-      for (let j = from; j <= to; j++) {
-        if (axis === 'X') {
-          const found = findY(marked, cur.from.y);
-          if (found !== -1) {
-            const foundX = findX(marked[[found]].x, j);
-            if (foundX !== -1) {
-              marked[[found]].x[[foundX]].count += 1;
-            } else {
-              marked[[found]].x.push({ val: j, count: 1 });
-            }
+  const filteredInput = input.filter((a) => a.from.x === a.to.x || a.from.y === a.to.y);
+
+  for (let i = 0; i < filteredInput.length; i++) {
+    const cur = filteredInput[i];
+
+    const axis = cur.from.x === cur.to.x ? 'Y' : 'X';
+    const from = axis === 'Y' ? cur.from.y : cur.from.x;
+    const to = axis === 'Y' ? cur.to.y : cur.to.x;
+
+    for (let j = from; j <= to; j++) {
+      if (axis === 'X') {
+        const found = findY(marked, cur.from.y);
+        if (found !== -1) {
+          const foundX = findX(marked[[found]].x, j);
+          if (foundX !== -1) {
+            marked[[found]].x[[foundX]].count += 1;
           } else {
-            marked.push({ y: cur.from.y, x: [{ val: j, count: 1 }] });
+            marked[[found]].x.push({ val: j, count: 1 });
           }
         } else {
-          let found = findY(marked, j);
+          marked.push({ y: cur.from.y, x: [{ val: j, count: 1 }] });
+        }
+      } else {
+        let found = findY(marked, j);
+        if (found !== -1) {
+          const foundX = findX(marked[[found]].x, cur.from.x);
+          if (foundX !== -1) {
+            marked[[found]].x[[foundX]].count += 1;
+          } else {
+            marked[[found]].x.push({ val: cur.from.x, count: 1 });
+          }
+        } else {
+          marked.push({ y: j, x: [] });
+          found = findY(marked, j);
+          marked[[found]].x.push({ val: cur.from.x, count: 1 });
+        }
+      }
+    }
+  }
+  return marked;
+}
+
+function processDiag(input, lines) {
+  const marked = lines;
+
+  const filteredInput = input.filter((a) => a.from.x !== a.to.x && a.from.y !== a.to.y);
+
+  for (let i = 0; i < filteredInput.length; i++) {
+    const cur = filteredInput[i];
+
+    if (cur.from.x < cur.to.x) {
+      // top left to bottom right
+      if (cur.from.y < cur.to.y) {
+        for (let { x } = cur.from, { y } = cur.from; x <= cur.to.x && y <= cur.to.y; x++, y++) {
+          let found = findY(marked, y);
           if (found !== -1) {
-            const foundX = findX(marked[[found]].x, cur.from.x);
+            const foundX = findX(marked[[found]].x, x);
             if (foundX !== -1) {
               marked[[found]].x[[foundX]].count += 1;
             } else {
-              marked[[found]].x.push({ val: cur.from.x, count: 1 });
+              marked[[found]].x.push({ val: x, count: 1 });
             }
           } else {
-            marked.push({ y: j, x: [] });
-            found = marked.findIndex((a) => a.y === j);
-            marked[[found]].x.push({ val: cur.from.x, count: 1 });
+            marked.push({ y, x: [] });
+            found = findY(marked, y);
+            marked[[found]].x.push({ val: x, count: 1 });
           }
+        }
+      } else {
+        for (let { x } = cur.from, { y } = cur.from; x <= cur.to.x && y >= cur.to.y; x++, y--) {
+          let found = findY(marked, y);
+          if (found !== -1) {
+            const foundX = findX(marked[[found]].x, x);
+            if (foundX !== -1) {
+              marked[[found]].x[[foundX]].count += 1;
+            } else {
+              marked[[found]].x.push({ val: x, count: 1 });
+            }
+          } else {
+            marked.push({ y, x: [] });
+            found = findY(marked, y);
+            marked[[found]].x.push({ val: x, count: 1 });
+          }
+        }
+      }
+    } else if (cur.from.y < cur.to.y) {
+      for (let { x } = cur.from, { y } = cur.from; x >= cur.to.x && y <= cur.to.y; x--, y++) {
+        let found = findY(marked, y);
+        if (found !== -1) {
+          const foundX = findX(marked[[found]].x, x);
+          if (foundX !== -1) {
+            marked[[found]].x[[foundX]].count += 1;
+          } else {
+            marked[[found]].x.push({ val: x, count: 1 });
+          }
+        } else {
+          marked.push({ y, x: [] });
+          found = findY(marked, y);
+          marked[[found]].x.push({ val: x, count: 1 });
+        }
+      }
+    } else {
+      for (let { x } = cur.from, { y } = cur.from; x >= cur.to.x && y >= cur.to.y; x--, y--) {
+        let found = findY(marked, y);
+        if (found !== -1) {
+          const foundX = findX(marked[[found]].x, x);
+          if (foundX !== -1) {
+            marked[[found]].x[[foundX]].count += 1;
+          } else {
+            marked[[found]].x.push({ val: x, count: 1 });
+          }
+        } else {
+          marked.push({ y, x: [] });
+          found = findY(marked, y);
+          marked[[found]].x.push({ val: x, count: 1 });
         }
       }
     }
   }
 
-  function findDupes(array) {
-    let totalDupes = 0;
-    array.forEach((elm) => {
-      elm.x.forEach((elm2) => {
-        if (elm2.count >= 2) {
-          totalDupes += 1;
-        }
-      });
-    });
+  return marked;
+}
 
-    return totalDupes;
-  }
+function findDupes(array) {
+  let totalDupes = 0;
+  array.forEach((elm) => {
+    elm.x.forEach((elm2) => {
+      if (elm2.count >= 2) {
+        totalDupes += 1;
+      }
+    });
+  });
+
+  return totalDupes;
+}
+
+function ProcessPart1(data) {
+  const input = splitCoods(data);
+
+  const marked = processHV(input);
+
+  const dupeCount = findDupes(marked);
+
+  // 6548
+  console.log(dupeCount);
+}
+
+function ProcessPart2(data) {
+  const input = splitCoods(data);
+
+  let marked = processHV(input);
+
+  marked = processDiag(input, marked);
 
   const dupeCount = findDupes(marked);
 
@@ -85,3 +184,4 @@ function ProcessPart1(data) {
 }
 
 load(filePath, ProcessPart1);
+load(filePath, ProcessPart2); // not 13354
