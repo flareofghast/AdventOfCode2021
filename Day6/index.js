2444cb9b4f48369e1f9b8c846d604285cefc769a
Peter Salzmann <petersalzmann@gmail.com>
2021-12-09 16:07:06 +1030

    add correct number in comment as result for part 2

--- Day6/index.js
+++ Day6/index.js
@@ -4,4 +4,4 @@ const lanternfish = require('./lanternfish');
 const filePath = '../Day6/input';
 
 load(filePath, (data) => { console.log(lanternfish.processPart1(data[0], 80)); }); // 361169
-load(filePath, (data) => { console.log(lanternfish.processPart2(data[0], 256)); });
+load(filePath, (data) => { console.log(lanternfish.processPart2(data[0], 256)); }); // 1634946868992
