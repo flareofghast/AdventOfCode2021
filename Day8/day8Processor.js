be08ca9aaeead722f46c3daf485e7972887814fe
Peter Salzmann <petersalzmann@gmail.com>
2021-12-22 09:58:46 +1030

    update day 8, haven't run test yet

---
+++ Day8/day8Processor.js
@@ -0,0 +1,41 @@
+const lengthOfOne = 2;
+const lengthOfFour = 4;
+const lengthOfSeven = 3;
+const lengthOfEight = 7;
+
+function splitInput(input) {
+  const toReturn = [];
+  input.forEach((element) => {
+    element.split('|')[1].split(' ').forEach((elm) => (elm === '' ? null : toReturn.push(elm)));
+  });
+
+  return toReturn;
+}
+
+function processPart1(input) {
+  const splitSorted = splitInput(input).sort((a, b) => a.length - b.length);
+  const counts = {
+    ones: 0, fours: 0, sevens: 0, eights: 0,
+  };
+
+  for (let i = 0, n = splitSorted.length; i < n; i++) {
+    switch (splitSorted[i].length) {
+      case lengthOfOne:
+        counts.ones += 1;
+        break;
+      case lengthOfFour:
+        counts.fours += 1;
+        break;
+      case lengthOfSeven:
+        counts.sevens += 1;
+        break;
+      case lengthOfEight:
+        counts.eights += 1;
+        break;
+      default: break;
+    }
+  }
+  console.log((counts.ones + counts.fours + counts.sevens + counts.eights));
+}
+
+module.exports = { processPart1 };
---
+++ Day8/index.js
@@ -0,0 +1,6 @@
+const day8 = require('./day8Processor');
+const loadInput = require('../common/loadfile');
+
+const filePath = '../Day8/input';
+
+loadInput(filePath, day8.processPart1);
