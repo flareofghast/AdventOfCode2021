f8a4fbda92fa7890fe78ef7cb45fd36af6ae38d3
Peter Salzmann <petersalzmann@gmail.com>
2021-12-24 14:50:10 +1030

    passed part 2 tests. haven't tested actual output

--- Day9/day9Processor.js
+++ Day9/day9Processor.js
@@ -71,4 +71,139 @@ function processPart1(input) {
   return { length: lowPoints.length, risk: riskCount };
 }
 
-module.exports = { processPart1 };
+function getBasinRecursiveYDown(element, basin, board) {
+  if (element === undefined || element.value == 9) {
+    return basin;
+  }
+  if (!basin.some((elm) => elm.x === element.x && elm.y === element.y)) {
+    basin.push(element);
+  }
+  if (element.y === board.length - 1) {
+    return basin;
+  }
+
+  return getBasinRecursiveYDown(board[element.y + 1][element.x], basin, board);
+}
+
+function getBasinRecursiveYUp(element, basin, board) {
+  if (element === undefined || element.value == 9) {
+    return basin;
+  }
+  if (!basin.some((elm) => elm.x === element.x && elm.y === element.y)) {
+    basin.push(element);
+  }
+  if (element.y === 0) {
+    return basin;
+  }
+
+  // for each X we want to check the Y (vertical)
+  let returnedX = basin;
+
+  if (element.x < board[0].length - 1) {
+    returnedX = getBasinRecursiveXRight(
+      board[element.y][element.x + 1],
+      basin,
+      board,
+    );
+  }
+
+  return getBasinRecursiveYUp(board[element.y - 1][element.x], basin, board);
+}
+
+function getBasinRecursiveXRight(element, basin, board) {
+  if (element === undefined || element.value == 9) {
+    return basin;
+  }
+  if (!basin.some((elm) => elm.x === element.x && elm.y === element.y)) {
+    basin.push(element);
+  }
+
+  // for each X we want to check the Y (vertical)
+  let returnedY = basin;
+
+  if (element.y < board.length - 1) {
+    returnedY = getBasinRecursiveYDown(
+      board[element.y + 1][element.x],
+      basin,
+      board,
+    );
+  }
+
+  if (element.y > 0) {
+    returnedY = getBasinRecursiveYUp(
+      board[element.y - 1][element.x],
+      basin,
+      board,
+    );
+  }
+
+  // return if can't go any further right
+  if (element.x === board[element.y].length - 1) {
+    return basin;
+  }
+
+  return getBasinRecursiveXRight(
+    board[element.y][element.x + 1],
+    returnedY,
+    board,
+  );
+}
+
+function getBasins(board) {
+  const basins = [];
+
+  // Do initial check of top line, if the first value is a 9 it will return empty.
+  for (let x = 0, m = board[0].length; x < m; x++) {
+    for (let y = 0, n = board.length; y < n; y++) {
+      let basinExists = true;
+      const basin = getBasinRecursiveXRight(board[y][x], [], board);
+
+      basin.sort((a, b) => Math.hypot(a.x, a.y) - Math.hypot(b.x, b.y));
+
+      // make sure the basin doesn't already exist in our list
+      if (basin.length > 0) {
+        if (basins.length > 0) {
+          for (let i = 0, o = basin.length; i < o; i++) {
+            const found = basins.find(
+              (bas) => bas.filter((elm) => elm.x === basin[i].x && elm.y === basin[i].y).length > 0,
+            );
+            if (found === undefined) {
+              basinExists = false;
+            } else {
+              break;
+            }
+          }
+        } else {
+          basinExists = false;
+        }
+
+        // if it doesn't exist add it to our list
+        if (basinExists === false) {
+          basins.push(basin);
+        }
+      }
+    }
+  }
+
+  return basins;
+}
+
+function processPart2(input) {
+  const board = populateBoard(input);
+  const basins = getBasins(board);
+
+  basins.sort((a, b) => b.length - a.length);
+
+  const top3 = basins.slice(0, 3);
+  let size = 1;
+
+  top3.forEach((bas) => {
+    size *= bas.length;
+  });
+
+  console.log(size);
+
+  return { basins, size };
+}
+
+module.exports = { processPart1, processPart2 };
--- tests/day9.spec.js
+++ tests/day9.spec.js
@@ -1,6 +1,6 @@
 const day9 = require('../Day9/day9Processor');
 
-describe('day9Processor.js', () => {
+describe('day9Processor.js Part1', () => {
   let input = [];
   beforeEach(() => { input = ['2199943210', '3987894921', '9856789892', '8767896789', '9899965678']; });
 
@@ -10,5 +10,18 @@ describe('day9Processor.js', () => {
 
   it('returns the correct risk count', () => {
     expect(day9.processPart1(input).risk).toBe(15);
+  });
+});
+
+describe('day9Processor.js Part2', () => {
+  let input = [];
+  beforeEach(() => { input = ['2199943210', '3987894921', '9856789892', '8767896789', '9899965678']; });
+
+  it('returns the correct amount of basins', () => {
+    expect(day9.processPart2(input).basins.length).toBe(4);
+  });
+
+  it('returns the correct value calculated by multiplying together the sizes of the three largest basins', () => {
+    expect(day9.processPart2(input).size).toBe(1134);
   });
 });
